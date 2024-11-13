3c57a5db1d64be32f14e2d8824331523065f0477
upadhyaydhruv <dmupadhyay12@gmail.com>
2024-11-13 08:45:17 -0500

    formatted the files

--- utilities.py
+++ utilities.py
@@ -1,87 +1,80 @@
 from math import atan2, asin, sqrt
 
-M_PI=3.1415926535
+M_PI = 3.1415926535
 
 import numpy as np
 
+
 class Logger:
-    
     def __init__(self, filename, headers=["e", "e_dot", "e_int", "stamp"]):
-        
         self.filename = filename
 
-        with open(self.filename, 'w') as file:
-            
-            header_str=""
+        with open(self.filename, "w") as file:
+            header_str = ""
 
             for header in headers:
-                header_str+=header
-                header_str+=", "
-            
-            header_str+="\n"
-            
+                header_str += header
+                header_str += ", "
+
+            header_str += "\n"
+
             file.write(header_str)
 
-
     def log_values(self, values_list):
-
-        with open(self.filename, 'a') as file:
-            vals_str=""
+        with open(self.filename, "a") as file:
+            vals_str = ""
             for value in values_list:
-                vals_str+=f"{value}, "
-            
-            vals_str+="\n"
-            
+                vals_str += f"{value}, "
+
+            vals_str += "\n"
+
             file.write(vals_str)
-            
 
     def save_log(self):
         pass
 
+
 class FileReader:
     def __init__(self, filename):
-        
         self.filename = filename
-        
-        
-    def read_file(self):
-        
-        read_headers=False
 
-        table=[]
-        headers=[]
-        with open(self.filename, 'r') as file:
+    def read_file(self):
+        read_headers = False
+
+        table = []
+        headers = []
+        with open(self.filename, "r") as file:
             # Skip the header line
             if not read_headers:
                 for line in file:
-                    values=line.strip().split(',')
+                    values = line.strip().split(",")
 
                     for val in values:
-                        if val=='':
+                        if val == "":
                             break
                         headers.append(val.strip())
 
-                    read_headers=True
+                    read_headers = True
                     break
-            
+
             next(file)
-            
+
             # Read each line and extract values
             for line in file:
-                values = line.strip().split(',')
-                
-                row=[]                
-                
+                values = line.strip().split(",")
+
+                row = []
+
                 for val in values:
-                    if val=='':
+                    if val == "":
                         break
                     row.append(float(val.strip()))
 
                 table.append(row)
-        
+
         return headers, table
-    
-    
+
+
 # Conversion from Quaternion to Euler Angles
 def euler_from_quaternion(quat):
     """
@@ -106,21 +99,22 @@ def euler_from_quaternion(quat):
 
 # Calculation of the linear error
 def calculate_linear_error(current_pose, goal_pose):
-        
-    return sqrt( (current_pose[0] - goal_pose[0])**2 +
-                (current_pose[1] - goal_pose[1])**2 )
+    return sqrt(
+        (current_pose[0] - goal_pose[0]) ** 2 + (current_pose[1] - goal_pose[1]) ** 2
+    )
+
 
 # Calculation of the angular error
 def calculate_angular_error(current_pose, goal_pose):
+    error_angular = (
+        atan2(goal_pose[1] - current_pose[1], goal_pose[0] - current_pose[0])
+        - current_pose[2]
+    )
 
-    error_angular= atan2(goal_pose[1]-current_pose[1],
-                        goal_pose[0]-current_pose[0]) - current_pose[2]
-    
     if error_angular <= -M_PI:
-        error_angular += 2*M_PI
-    
-    
+        error_angular += 2 * M_PI
+
     elif error_angular >= M_PI:
-        error_angular -= 2*M_PI
-    
+        error_angular -= 2 * M_PI
+
     return error_angular
